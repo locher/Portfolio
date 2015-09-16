1209360c505736131164541e0060163c4daf7c37
Loïc H <loic@herrloc.fr>
2015-09-16 14:03:49 +0200

    Factorisation liste folio

---
+++ list-folio.php
@@ -0,0 +1,35 @@
+<?php 
+	$portfolio = new WP_Query(array(
+		'post_type'=>'portfolio'
+	));
+?>
+
+<?php if ($portfolio->have_posts()): ?>
+
+<section class="wrapper-folio">
+
+	<?php while ($portfolio->have_posts()) : $portfolio->the_post(); ?>
+
+	<?php $img_desktop = get_field('image_desktop'); ?>
+
+	<a href="<?php the_permalink(); ?>" class="pad-folio" style="background-color: <?php the_field('couleur_de_fond'); ?>">
+
+		<div class="wrapper-img">
+
+			<?php if($img_desktop): ?>
+				<div class="img-desktop" style="background-image: url('<?php echo $img_desktop['url']; ?>')">
+					<img src="<?php echo $img_desktop['url']; ?>">
+				</div>					
+			<?php endif; ?>
+			
+		</div>
+
+		<h2><?php the_field('titre_home'); ?></h2>
+		<p><?php the_field('sous_titre_home'); ?></p>
+	</a>
+
+	<?php endwhile; ?>
+
+</section>
+
+<?php endif; ?>
--- template-portfolio.php
+++ template-portfolio.php
@@ -27,41 +27,7 @@
 
 	<main role="main">
 
-		<?php 
-			$portfolio = new WP_Query(array(
-				'post_type'=>'portfolio'
-			));
-		?>
-
-		<?php if ($portfolio->have_posts()): ?>
-
-		<section class="wrapper-folio">
-
-			<?php while ($portfolio->have_posts()) : $portfolio->the_post(); ?>
-
-			<?php $img_desktop = get_field('image_desktop'); ?>
-
-			<a href="<?php the_permalink(); ?>" class="pad-folio" style="background-color: <?php the_field('couleur_de_fond'); ?>">
-
-				<div class="wrapper-img">
-
-					<?php if($img_desktop): ?>
-						<div class="img-desktop" style="background-image: url('<?php echo $img_desktop['url']; ?>')">
-							<img src="<?php echo $img_desktop['url']; ?>">
-						</div>					
-					<?php endif; ?>
-					
-				</div>
-
-				<h2><?php the_field('titre_home'); ?></h2>
-				<p><?php the_field('sous_titre_home'); ?></p>
-			</a>
-
-			<?php endwhile; ?>
-
-		</section>
-
-		<?php endif; ?>
+		<?php include('list-folio.php'); ?>
 
 	</main>
 
