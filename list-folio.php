b6471f14513682d5acb05c65312ee8e8c8add19a
Loïc H <loic@herrloc.fr>
2015-09-16 14:21:37 +0200

    Ajout tablette portfolio

--- list-folio.php
+++ list-folio.php
@@ -10,7 +10,10 @@
 
 	<?php while ($portfolio->have_posts()) : $portfolio->the_post(); ?>
 
-	<?php $img_desktop = get_field('image_desktop'); ?>
+	<?php 
+		$img_desktop = get_field('image_desktop');
+		$img_tablette = get_field('image_tablette');
+	?>
 
 	<a href="<?php the_permalink(); ?>" class="pad-folio" style="background-color: <?php the_field('couleur_de_fond'); ?>">
 
@@ -19,6 +22,12 @@
 			<?php if($img_desktop): ?>
 				<div class="img-desktop" style="background-image: url('<?php echo $img_desktop['url']; ?>')">
 					<img src="<?php echo $img_desktop['url']; ?>">
+				</div>					
+			<?php endif; ?>
+
+			<?php if($img_tablette): ?>
+				<div class="img-tablette" style="background-image: url('<?php echo $img_tablette['url']; ?>')">
+					<img src="<?php echo $img_tablette['url']; ?>">
 				</div>					
 			<?php endif; ?>
 			
