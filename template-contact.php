20ed8a76794573472ad6918d65dd6060e870d073
Loïc Herr <loic@herrloc.fr>
2019-12-29 17:34:59 +0100

    Contact

--- template-contact.php
+++ template-contact.php
@@ -16,50 +16,6 @@
 			<?php echo do_shortcode(get_field('formulaire_contact')); ?>
 		</div>
 
-		<div class="about-pads contact-pads">
-
-			<div class="pad-argument">
-				<div>
-					<svg viewBox="0 0 100 100" class="icon">
-						<use xlink:href="#icon-phone"></use>
-					</svg>
-					<h2>Téléphone</h2>
-					<p><a href="tel:<?php the_field('telephone'); ?>"><?php the_field('telephone'); ?></a></p>
-				</div>
-			</div>
-
-			<div class="pad-argument">
-				<div>
-					<svg viewBox="0 0 100 100" class="icon">
-						<use xlink:href="#icon-mail"></use>
-					</svg>
-					<h2>Mail</h2>
-					<p><a href="mailto:<?php the_field('mail'); ?>"><?php the_field('mail'); ?></a></p>
-				</div>
-			</div>
-
-			<div class="pad-argument">
-				<div>
-					<svg viewBox="0 0 100 100" class="icon">
-						<use xlink:href="#icon-address"></use>
-					</svg>
-					<h2>Adresse</h2>
-					<p><?php the_field('adresse'); ?></p>
-				</div>
-			</div>
-
-
-
-			<a href="<?php echo site_url(); ?>/realisations" class="contact">
-				<div>				
-					<svg viewBox="0 0 100 100" class="icon">
-						<use xlink:href="#icon-portfolio"></use>
-					</svg>
-					<span>Réalisations</span>
-				</div>
-			</a>
-		</div>
-
 	</main>
 
 <?php get_footer(); ?>
