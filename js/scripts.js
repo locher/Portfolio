5e142e4fc87efd26450761dc9f660a17125c1fd8
Loïc Herr <loic@herrloc.fr>
2019-12-30 14:19:04 +0100

    Anims effect

--- js/scripts.js
+++ js/scripts.js
@@ -74,12 +74,14 @@ scrollvitesse = function(){
 //Animations
 
 window.sr = ScrollReveal({mobile:false});
-//sr.reveal('.wrapper-top');
-sr.reveal('.big-header h1');
-sr.reveal('.sous-title');
-sr.reveal('.big-title');
+sr.reveal('.wrapper-top > *', {delay: 200}, 300);
+sr.reveal('.big-title', {delay: 400});
+sr.reveal('.sous-title', {delay: 500});
+sr.reveal('.infos > *',{delay: 600},  50);
+
+
 sr.reveal('.pad-argument', 50);
-sr.reveal('.infos > *', 50);
+
 sr.reveal('.nav_secondary li', 50);
 sr.reveal('.wrapper-folio a', { delay: 100, distance: '100px' });
 sr.reveal('.offre-wrapper > *', 100);
